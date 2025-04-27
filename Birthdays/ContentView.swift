ace36ee679a8cffe394b81e6e252825e7bba25b7
christineagatha <christinejiang09@gmail.com>
2025-04-27 02:47:14 -0500

    in progress (having issues with model and query)

--- Birthdays/ContentView.swift
+++ Birthdays/ContentView.swift
@@ -6,19 +6,17 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
-    @State private var friends: [Friend] = [
-        // I'm not sure if we are supposed to calculate the correct birthday
-        Friend(name: "Christine Jiang", birthday: Date(timeIntervalSinceReferenceDate: 12345)),
-        Friend(name: "Emily Iorio", birthday: Date(timeIntervalSinceReferenceDate: 67890 ))
-    ]
+    @Query private var friends: [Friend]
+    @Environment(\.modelContext) private var modelContext
     @State private var newName = ""
     @State private var newBirthday = Date.now
     
     var body: some View {
         NavigationStack {
-            List(friends, id: \.name) { friend in
+            List(friends) { friend in
                 HStack {
                     Text(friend.name)
                     Text(friend.birthday,format: .dateTime.month(.wide).day().year())
@@ -33,9 +31,9 @@ struct ContentView: View {
                     }
                     Button("Save") {
                         let newFriend = Friend(name: newName, birthday: newBirthday)
-                        friends.append(newFriend)
+                        modelContext.insert(newFriend)
                         newName = ""
-                        newBirthday = Date.now
+                        newBirthday = .now
                     }
                 }
                 .padding()
@@ -47,4 +45,5 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+        .modelContainer(for: Friend.self, inMemory: true)
 }
