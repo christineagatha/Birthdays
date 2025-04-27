bf6f2472f65e49a9b0de4a51037ea0cd58e415a6
christineagatha <christinejiang09@gmail.com>
2025-04-27 02:06:29 -0500

    created existing friends and added the names and birthdays of new friends in list

--- Birthdays/ContentView.swift
+++ Birthdays/ContentView.swift
@@ -8,14 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var friends: [Friend] = [
+        // I'm not sure if we are supposed to calculate the correct birthday
+        Friend(name: "Christine Jiang", birthday: Date(timeIntervalSinceReferenceDate: 12345)),
+        Friend(name: "Emily Iorio", birthday: Date(timeIntervalSinceReferenceDate: 67890 ))
+    ]
+    @State private var newName = ""
+    @State private var newBirthday = Date.now
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(friends, id: \.name) { friend in
+                HStack {
+                    Text(friend.name)
+                    Text(friend.birthday,format: .dateTime.month(.wide).day().year())
+                }
+            }
+            .navigationTitle("Birthdays")
+            .safeAreaInset(edge: .bottom) {
+                VStack(spacing: 5) {
+                    Text("New Birthday")
+                    DatePicker(selection: $newBirthday) {
+                        TextField("Name", text: $newName)
+                    }
+                    Button("Save") {
+                        let newFriend = Friend(name: newName, birthday: newBirthday)
+                        friends.append(newFriend)
+                        newName = ""
+                        newBirthday = Date.now
+                    }
+                }
+                .padding()
+                .background(.gray.opacity(0.3))
+            }
         }
-        .padding()
     }
 }
 
